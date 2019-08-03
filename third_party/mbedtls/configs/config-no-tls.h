0ddf76377ec8c879280390e055e59e59e36eef09
aggresss <aggresss@163.com>
2019-08-03 17:42:49 +0800

    2019-08-03 17:42:49 CST W30D6

---
+++ third_party/mbedtls/configs/config-no-tls.h
@@ -0,0 +1,24 @@
+/**
+ * \file config-no-tls.h
+ *
+ */
+
+#ifndef MBEDTLS_CONFIG_H
+#define MBEDTLS_CONFIG_H
+
+/* System support */
+#define MBEDTLS_HAVE_ASM
+#define MBEDTLS_HAVE_TIME
+
+
+/* mbed TLS modules */
+#define MBEDTLS_AES_C
+#define MBEDTLS_MD_C
+#define MBEDTLS_MD5_C
+#define MBEDTLS_SHA1_C
+#define MBEDTLS_BASE64_C
+
+#include "mbedtls/check_config.h"
+
+#endif /* MBEDTLS_CONFIG_H */
+
